cf8f5ccaebe0a55fe2ca1164de565257c241e7e5
Inho Choi <eknow25@gmail.com>
2022-06-02 05:37:00 +0900

    [Feat] 에어테이블 접근 및 조회 기능 추가

--- AzitQRCodeReceiver/ContentView.swift
+++ AzitQRCodeReceiver/ContentView.swift
@@ -8,9 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+	private let airtable = AirTalbe()
+	
     var body: some View {
         Text("Hello, world!")
             .padding()
+			.onAppear {
+				airtable.getRecords()
+			}
     }
 }
 
