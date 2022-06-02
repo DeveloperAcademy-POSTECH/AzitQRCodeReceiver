ca64d93cfeae9733a7fd15f3218ec81a1a3bf14d
Inho Choi <eknow25@gmail.com>
2022-06-02 21:59:24 +0900

    [Feat] QRCode Camera Settings

--- AzitQRCodeReceiver/ContentView.swift
+++ AzitQRCodeReceiver/ContentView.swift
@@ -6,19 +6,40 @@
 //
 
 import SwiftUI
+import CodeScanner
 
 struct ContentView: View {
 	private let airtable = AirTalbe()
+	@State var session : String = ""
+	@State var name : String = ""
 	
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-			.onAppear {
-//				airtable.attendanceCheck(name: "Toby(최인호)", session: "Afternoon Session")
-//				print(airtable.records.afternoonRecords.count + airtable.records.morningRecords.count + airtable.records.mentorsopsRecords.count)
-			}
-    }
-}
+		VStack {
+			CodeScannerView(codeTypes: [.qr],scanMode: .oncePerCode, videoCaptureDevice: .default(for: .video)) { response in
+				switch response {
+				case .success(let result):
+					print("Found code: \(result.string)")
+					let arrayResult = result.string.components(separatedBy: ",")
+				
+					self.session = arrayResult[0].trimmingCharacters(in: .whitespaces)
+					self.name = arrayResult[1].trimmingCharacters(in: .whitespaces)
+					airtable.attendanceCheck(name: self.name, session: self.session)
+					
+				case .failure(let error):
+					print(error.localizedDescription)
+				}
+			}// CodeScannerView
+			
+			Group {
+				Text("Name : \(name)")
+					.padding()
+				Text("Session : \(session)")
+					.padding()
+			} // Group
+			
+		}// VStack
+    } // body
+} // Struct
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
